16f37bba40dc559a37ada99a946ec5684fac3916
mikhmol <mikhailo.moliakov@gmail.com>
2023-01-15 20:03:09 +0200

    Updated README

--- publish.sh
+++ publish.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/chebelok/db-course-work-media-analyze master:gh-pages
+git push -f https://github.com/mikhmol/db-course-work-media-analyze.git master:gh-pages
 
 cd -
